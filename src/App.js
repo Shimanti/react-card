2536c01a46d618a187bc23288061ed43557da85a
Shimanti <shim2022@Shimantis-MBP.hsd1.az.comcast.net>
2022-03-10 13:56:03 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./style.css"
 
-function App() {
+export default function App() {
+  const [contact, setContact] = React.useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: true
+  })
+
+  let starIcon = contact.isFavorite ? "star-empty.png" : "star-filled.png"
+  function toggleStar() {
+    setContact(prevContact => ({
+      ...prevContact,
+      isFavorite: !prevContact.isFavorite
+    }))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div className="card">
+        <img src="../images/user.png" className="card--image"/>
+        <div className="card--info">
+          <img src={`../images/${starIcon}`}
+            className="card--favorite"
+            onClick={toggleStar}/>
+          <h2 className="card--name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="card--contact">{contact.phone}</p>
+          <p className="card--contact">{contact.email}</p>
+        </div>
+      </div>
+    </main>
+  )
 }
-
-export default App;
